1b363195cb88c8a43b7151eb00cd58ea62006ef1
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-10-22 10:12:59 +0900

    -

--- src/bin/rzip/main.rs
+++ src/bin/rzip/main.rs
@@ -7,14 +7,93 @@ mod archiver;
 mod configuration;
 mod util;
 
-/// Show usage.
-fn usage(options: &getopts::Options) {
-	eprintln!("RZIP: Recursively compress files and directories.");
-	eprintln!("    * Recursively compresses all files and directories under the specified path.");
-	eprintln!("    * If the specified path is a directory, the directory name is used as the root of the archive.");
-	eprintln!();
-	eprint!("{}", options.short_usage("\n    rzip \"archived.zip\" \"path to archive\""));
-	eprint!("{}", options.usage(""));
+struct CommandlineOptions {
+	// Core options.
+	options: getopts::Options,
+
+	/// Option: --help
+	pub help: bool,
+
+	/// Option: --root
+	pub root: bool,
+
+	/// Option: --sleep
+	pub sleep: Option<f32>,
+
+	/// Others.
+	pub free: Vec<String>,
+}
+
+impl CommandlineOptions {
+	/// Create new instance.
+	pub fn new() -> Self {
+		let mut options = getopts::Options::new();
+		options.opt("h", "help", "Usage.", "", getopts::HasArg::No, getopts::Occur::Optional);
+		options.opt("", "root", "No root folder.", "", getopts::HasArg::No, getopts::Occur::Optional);
+		options.opt(
+			"",
+			"sleep",
+			"Sleep {n} seconds after finish.",
+			"NUMBER",
+			getopts::HasArg::Yes,
+			getopts::Occur::Optional,
+		);
+
+		let instance = CommandlineOptions {
+			options: options,
+			help: false,
+			root: false,
+			sleep: None,
+			free: Vec::new(),
+		};
+
+		return instance;
+	}
+
+	/// Parse commandline options.
+	pub fn parse(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
+		// Commandline options.
+		let args: Vec<String> = std::env::args().skip(1).collect();
+
+		let matches = self.options.parse(args)?;
+
+		// Option: --help
+		self.help = matches.opt_present("help");
+
+		// Option: --root
+		self.root = matches.opt_present("root");
+
+		// Option: --sleep
+		if matches.opt_present("sleep") {
+			let value = matches.opt_str("sleep").unwrap();
+			let value = value.parse::<f32>();
+			if value.is_err() {
+				return Err("Invalid value for option: '--sleep'".into());
+			}
+			self.sleep = Some(value.unwrap());
+		};
+
+		// Free options.
+		self.free = matches.free;
+
+		return Ok(());
+	}
+
+	pub fn free(&self) -> &Vec<String> {
+		return &self.free;
+	}
+
+	/// Show usage.
+	pub fn usage(&self) {
+		let options = &self.options;
+
+		eprintln!("RZIP: Recursively compress files and directories.");
+		eprintln!("    * Recursively compresses all files and directories under the specified path.");
+		eprintln!("    * If the specified path is a directory, the directory name is used as the root of the archive.");
+		eprintln!();
+		eprint!("{}", options.short_usage("\n    rzip \"archived.zip\" \"path to archive\""));
+		eprint!("{}", options.usage(""));
+	}
 }
 
 /// Entrypoint.
@@ -27,60 +106,35 @@ fn main() {
 	}
 	let settings = result.unwrap();
 
-	// Commandline options.
-	let args: Vec<String> = std::env::args().skip(1).collect();
-
 	// Parse commandline options.
-	let mut options = getopts::Options::new();
-	options.opt("h", "help", "Usage.", "", getopts::HasArg::No, getopts::Occur::Optional);
-	options.opt("", "root", "No root folder.", "", getopts::HasArg::No, getopts::Occur::Optional);
-	options.opt(
-		"",
-		"sleep",
-		"Sleep {n} seconds after finish.",
-		"NUMBER",
-		getopts::HasArg::Yes,
-		getopts::Occur::Optional,
-	);
-	let result = options.parse(args);
+	let mut options = CommandlineOptions::new();
+	let result = options.parse();
 	if result.is_err() {
 		eprintln!("{}", result.err().unwrap());
 		eprintln!();
-		usage(&options);
+		options.usage();
 		std::process::exit(1);
 	}
-	let matches = result.unwrap();
 
-	if matches.opt_present("h") {
-		usage(&options);
+	// Show usage.
+	if options.help {
+		options.usage();
 		return;
 	}
 
 	// Sleep after finish.
-	let sleep_seconds: f32 = if matches.opt_present("sleep") {
-		let value = matches.opt_str("sleep").unwrap();
-		let value = value.parse::<f32>();
-		if value.is_err() {
-			eprintln!("Invalid value for option: '--sleep'");
-			eprintln!();
-			usage(&options);
-			std::process::exit(1);
-		}
-		value.unwrap()
-	} else {
-		0.0
-	};
+	let sleep_seconds: f32 = options.sleep.unwrap_or(0.0);
 	let milliseconds = sleep_seconds * 1000.0;
 	let milliseconds = milliseconds as u64;
 
 	// Optional: No root directory.
-	let create_root = !matches.opt_present("root");
+	let create_root = !options.root;
 
 	// Free options.
-	let free_args = matches.free;
+	let free_args = options.free();
 
 	if free_args.len() < 2 {
-		usage(&options);
+		options.usage();
 		std::process::exit(1);
 	}
 
